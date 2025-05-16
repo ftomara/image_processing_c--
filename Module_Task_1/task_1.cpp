bfe6dbe5594b6f0bc4a0692f90b33b4fccf09044
ftomara <ftomara647@gmail.com>
2025-05-16 19:20:22 +0300

    added the code for the 4 channels

--- Module_Task_1/task_1.cpp
+++ Module_Task_1/task_1.cpp
@@ -4,17 +4,42 @@
 #include "/Users/fatmaomara/Desktop/image_processing_c++/stb_image.h"
 #include "/Users/fatmaomara/Desktop/image_processing_c++/stb_image_write.h"
 using namespace std;
+void manipulatePixels(vector<vector<int>> &v)
+{
+    int height = v.size(), width = v.front().size();
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int avg = 0;
+            for (int x = i - 1; x <= i + 1; x++)
+            {
+                for (int y = j - 1; y <= j + 1; y++)
+                {
+                    if (x >= 0 && x < height && y >= 0 && y < width && !(x == i && y == j))
+                    {
+                        avg += v[x][y];
+                    }
+                }
+            }
+            v[i][j] = avg / 8;
+        }
+    }
+}
 int main()
 {
     int width, height, channels;
-    unsigned char *img = stbi_load("small-2.bmp", &width, &height, &channels, 0);
+    unsigned char *img = stbi_load("shifo.bmp", &width, &height, &channels, 0);
     if (img == NULL)
     {
         cout << "Error loading image" << endl;
         return 1;
     }
     cout << "Image loaded successfully: " << width << "x" << height << ", " << channels << " channels" << endl;
-    vector<vector<int>> v(height, vector<int>(width));
+    vector<vector<int>> red(height, vector<int>(width));
+    vector<vector<int>> green(height, vector<int>(width));
+    vector<vector<int>> blue(height, vector<int>(width));
+    vector<vector<int>> alpha(height, vector<int>(width));
 
     // getting red channel pixels
     for (int i = 0; i < height; i++)
@@ -22,58 +47,17 @@ int main()
         for (int j = 0; j < width; j++)
         {
             int index = (width * i + j) * channels;
-            v[i][j] = (int)img[index];
+            red[i][j] = (int)img[index];
+            green[i][j] = (int)img[index + 1];
+            blue[i][j] = (int)img[index + 2];
+            alpha[i][j] = (int)img[index + 3];
         }
     }
-
+    manipulatePixels(red);
+    manipulatePixels(green);
+    manipulatePixels(blue);
+    manipulatePixels(alpha);
     // manipulate pixels to there neighbours avg
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            int avg;
-            if (j == 0 && (i != 0 && i != height - 1))
-            {
-                avg = v[i - 1][j] + v[i - 1][j + 1] + v[i][j + 1] + v[i + 1][j] + v[i + 1][j + 1];
-            }
-            else if (j == width - 1 && (i != 0 && i != height - 1))
-            {
-                avg = v[i - 1][j - 1] + v[i - 1][j] + v[i][j - 1] + v[i + 1][j - 1] + v[i + 1][j];
-            }
-            else if (i == 0 && (j != 0 && j != width - 1))
-            {
-                avg = v[i][j - 1] + v[i][j + 1] + v[i + 1][j - 1] + v[i + 1][j] + v[i + 1][j + 1];
-            }
-            else if (i == height - 1 && (j != 0 && j != width - 1))
-            {
-                avg = v[i - 1][j - 1] + v[i - 1][j] + v[i - 1][j + 1] + v[i][j - 1] + v[i][j + 1];
-            }
-            else if (j == 0 && i == 0)
-            {
-                avg = v[i][j + 1] + v[i + 1][j] + v[i + 1][j + 1];
-            }
-            else if (j == 0 && i == height - 1)
-            {
-                // j-1 , i+1
-                avg = v[i - 1][j] + v[i - 1][j + 1] + v[i][j + 1];
-            }
-            else if (j == width - 1 && i == 0)
-            {
-                // j+1 , i-1
-                avg = v[i][j - 1] + v[i + 1][j - 1] + v[i + 1][j];
-            }
-            else if (j == width - 1 && i == height - 1)
-            {
-                // j+1 ,i+1
-                avg = v[i - 1][j - 1] + v[i - 1][j] + v[i][j - 1];
-            }
-            else
-            {
-                avg = v[i - 1][j - 1] + v[i - 1][j] + v[i - 1][j + 1] + v[i][j - 1] + v[i][j + 1] + v[i + 1][j - 1] + v[i + 1][j] + v[i + 1][j + 1];
-            }
-            v[i][j] = avg / 8;
-        }
-    }
 
     // image writing
     for (int i = 0; i < height; i++)
@@ -81,10 +65,13 @@ int main()
         for (int j = 0; j < width; j++)
         {
             int index = (width * i + j) * channels;
-            img[index] = (char)v[i][j];
+            img[index] = (char)red[i][j];
+            img[index + 1] = (char)green[i][j];
+            img[index + 2] = (char)blue[i][j];
+            img[index + 3] = (char)alpha[i][j];
         }
     }
-    if (!stbi_write_bmp("output.bmp", width, height, channels, img))
+    if (!stbi_write_bmp("shifo_out.bmp", width, height, channels, img))
     {
         cout << "Error writing image" << endl;
     }
